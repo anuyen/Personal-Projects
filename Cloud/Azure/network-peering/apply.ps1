9c2851314780ab550ca72c04c8810b089c83ec3e
antnguyen72 <109638158+antnguyen72@users.noreply.github.com>
2023-12-30 23:14:46 -0600

    documentation for script

--- Cloud/Azure/network-peering/apply.ps1
+++ Cloud/Azure/network-peering/apply.ps1
@@ -4,6 +4,20 @@
 $startTime = Get-Date
 
 function Invoke-Async-Commands {
+    <#
+    Description:
+        This function takes in paths and commands.
+        If more than one paths is given, the series of commands are started asynchronously in each folder.
+        The series of commands themselves are ran sequentially.
+
+        The function will wait for jobs to finish and write out output before removing the job and exiting the function
+    Variable:
+        paths: input - an array of paths
+        commands: input - an array of commands
+    Use:
+        With async function call, the duration of the function will as long as the longest single-function
+        durction
+    #>
     param(
         [string[]]$paths,
         [string[]]$commands
@@ -11,12 +25,17 @@ function Invoke-Async-Commands {
 
     $jobs = @()
     foreach ($path in $paths){
-        # When Invoke-Expression is used, multiple commands sequentially can be
-        # invoked if they have separed by ; in that string
+        # When Invoke-Expression is used, multiple commands can be
+        # sequentially invoked if they have separated by "";"" in that string
+
+        # This variable will collect commands in the following manner
+        # "cmd1,cmd2,..."
         $final_commands = ""
         foreach ($command in $commands){
             $final_commands += "terraform -chdir=`"$path`" $command;"
         }
+
+        # Start job
         $jobs += Start-Job -ScriptBlock {
             param($final_commands)
                 Write-Host "-----------------------------------------------------------------" -ForegroundColor Cyan
@@ -24,9 +43,10 @@ function Invoke-Async-Commands {
                 Write-Host $final_commands -ForegroundColor Cyan
                 Invoke-Expression $final_commands
         } -ArgumentList $final_commands
-    }
+    } #This block will iteratively start each job, but does not wait for the last job to finish
+    #Before starting another job --> they are essentially started at the same time
 
-    # Wait
+    # Wait for jobs to be done
     Wait-Job $jobs > $null
     do {
         foreach ($job in $jobs) {
@@ -35,11 +55,10 @@ function Invoke-Async-Commands {
             } else {
                 $result = Receive-Job $job
                 Write-Output $result  # Print the result to the terminal
-                # Process result if needed
             }
         }
-    }
-    while (($jobs | Where-Object HasMoreData).Count -gt 0)
+    } # Waiting for job to be done is not enough, I found that completed jobs are not really completed
+    while (($jobs | Where-Object HasMoreData).Count -gt 0) # checks if they still got outputs
 
     $jobs | Remove-Job
 }
